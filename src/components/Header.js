131422534eab6ee22a17171c165b1b641bddec6e
jimmy <jh5397@nyu.edu>
2020-08-26 01:48:31 -0700

    Responsive design

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Header() {
     return (
         <div className="headerContainer">
-            <p>『 CSS Sketchbook 』</p>
+            <div className="headerBig">
+                <p>『 CSS Sketchbook 』</p>
+            </div>
+            <div className="headerSmall">
+                <p><a target="_blank" href="https://jimmyhwang.me">Jimmy Hwang</a> ⇋ <a target="_blank" href="https://github.com/jimmyhwang4/css-sketchbook">Github</a></p>
+            </div>
         </div>
     );
 }
